b517d6302f184600875776677bffce4994fee55e
naimsadiq <nayemsadiq2013@gmail.com>
2025-09-08 00:01:50 +0600

    modal show function create done

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,5 +1,36 @@
 
-const getID = (id) => getElement = document.getElementById(id);
+const cartConteinar = document.getElementById('cart-conteiner');
+
+cartConteinar.addEventListener('click', (evt) => {
+    if(evt.target.localName === 'h3'){
+        // console.log(evt.target.id)
+        showModal(evt.target.id)
+    }
+    if(evt.target.localName === 'button'){
+        console.log(evt.target)
+    }
+})
+
+
+const showModal = (id) => {
+    document.getElementById('my_modal_5').showModal();
+    // console.log(id);
+const plantsDetailUrl = `https://openapi.programming-hero.com/api/plant/${id}`;
+    fetch(plantsDetailUrl)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.plants);
+        const plants = data.plants;
+        const modalCart = document.getElementById('modal-cart');
+        modalCart.innerHTML = `
+            <h3 class="text-xl font-semibold">${plants.name}</h3>
+            <img class="w-full h-[220px] object-cover rounded-md" src="${plants.image}" alt="">
+            <span class=" block"><b>Category: </b>${plants.category}</span>
+            <span class="block"><b>Price: </b>৳${plants.price}</span>
+            <p class=""><b>Description: </b>${plants.description}</p>
+        `
+})
+}
 
 // All Plants Conteinar function 
 const allPlantsUrl = 'https://openapi.programming-hero.com/api/plants';
@@ -10,13 +41,14 @@ fetch(allPlantsUrl)
 })
 
 const allPlantsShow = (allPlants) => {
-    const cartConteinar = getID('cart-conteiner')
+    // const cartConteinar = getID('cart-conteiner')
     allPlants.forEach(plant => {
+        // console.log(plant.id)
         cartConteinar.innerHTML += `
             <div class="cart bg-[#FFFFFF] rounded-lg p-4 space-y-3">
                     <img class="w-full h-40 object-cover rounded-md" src="${plant.image}" alt="">
                     <div class="cart-content space-y-2">
-                        <h3 class="text-[14px] text-[#1F2937] font-semibold">${plant.name}</h3>
+                        <h3 id="${plant.id}" class="text-[14px] text-[#1F2937] font-semibold">${plant.name}</h3>
                         <p class="text-[12px] text-[#1F2937] line-clamp-3">${plant.description}</p>
                         <div class="flex justify-between">
                             <span class="text-sm text-[#15803D] font-medium bg-[#DCFCE7] py-[6px] px-3 rounded-2xl">${plant.category}</span>
@@ -40,7 +72,7 @@ fetch(categoriesUrl)
 
 const categoriesShow = (categories) => {
     // console.log(categories);
-    const categoryContainer = getID('categories-conteinar');
+    const categoryContainer = document.getElementById('categories-conteinar');
     categories.forEach(category => {
         categoryContainer.innerHTML +=`
             <li id="${category.id}" class="categories-btn font-medium text-black hover:text-[#FFFFFF] hover:bg-[#15803D] p-2 mb-1 rounded-lg cursor-pointer">${category.category_name}</li>
@@ -67,20 +99,19 @@ const categoryCart = (id) => {
     fetch(categoryCartUrl)
     .then(response => response.json())
     .then(data => {
-        console.log(data.plants);
+        // console.log(data.plants);
         showCategoryCart(data.plants)
     })
 }
 
 const showCategoryCart = (plants) => {
-    const cartConteinar = getID('cart-conteiner')
     cartConteinar.innerHTML = '';
     plants.forEach(plant => {
         cartConteinar.innerHTML += `
             <div class="cart bg-[#FFFFFF] rounded-lg p-4 space-y-3">
                     <img class="w-full h-40 object-cover rounded-md" src="${plant.image}" alt="">
                     <div class="cart-content space-y-2">
-                        <h3 class="text-[14px] text-[#1F2937] font-semibold">${plant.name}</h3>
+                        <h3 id="${plant.id}" class="text-[14px] text-[#1F2937] font-semibold">${plant.name}</h3>
                         <p class="text-[12px] text-[#1F2937] line-clamp-3">${plant.description}</p>
                         <div class="flex justify-between">
                             <span class="text-sm text-[#15803D] font-medium bg-[#DCFCE7] py-[6px] px-3 rounded-2xl">${plant.category}</span>
@@ -92,3 +123,4 @@ const showCategoryCart = (plants) => {
         `
     })
 }
+
